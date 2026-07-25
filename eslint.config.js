5a6e16824f5cfd275dfc247f7c82af199c082fd1
Daniil <dadyarri@gmail.com>
2026-07-25 13:36:15 +0300

    introduce auto sorting imports

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import globals from "globals";
 import tseslint from "typescript-eslint";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 export default tseslint.config(
   { ignores: ["dist"] },
@@ -20,6 +21,7 @@ export default tseslint.config(
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
+      "simple-import-sort": simpleImportSort,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -31,6 +33,7 @@ export default tseslint.config(
       "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
       "@typescript-eslint/only-throw-error": "off",
       "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+      "simple-import-sort/imports": "warn",
     },
   },
 );
